1263ce14822e98dad07210cf231bcaab95c773fd
nowshad-islam-dev <mdnowshadislam.2108033.ruet.mte@gmail.com>
2025-05-28 22:47:00 +0600

    initial

---
+++ app/index.tsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import { Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Link } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const Home = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <MaterialCommunityIcons name="account-group" size={80} color="#F9405F" />
+
+      <Text style={styles.title}>Manage your queues</Text>
+      <Text style={styles.subtitle}>
+        Join virtual queues for various services and service providers.
+      </Text>
+
+      <Link href="/login" asChild>
+        <TouchableOpacity style={styles.loginButton}>
+          <Text style={styles.loginText}>Log in</Text>
+        </TouchableOpacity>
+      </Link>
+
+      <Link href="/register" asChild>
+        <TouchableOpacity style={styles.registerButton}>
+          <Text style={styles.registerText}>Register</Text>
+        </TouchableOpacity>
+      </Link>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 24,
+    paddingTop: 80,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '700',
+    textAlign: 'center',
+    marginVertical: 16,
+  },
+  subtitle: {
+    fontSize: 16,
+    color: '#444',
+    textAlign: 'center',
+    marginBottom: 32,
+  },
+  loginButton: {
+    backgroundColor: '#F9405F',
+    paddingVertical: 14,
+    borderRadius: 12,
+    width: '100%',
+    marginBottom: 12,
+  },
+  loginText: {
+    color: '#fff',
+    textAlign: 'center',
+    fontWeight: '600',
+    fontSize: 16,
+  },
+  registerButton: {
+    backgroundColor: '#F7F3F4',
+    paddingVertical: 14,
+    borderRadius: 12,
+    width: '100%',
+  },
+  registerText: {
+    color: '#000',
+    textAlign: 'center',
+    fontWeight: '600',
+    fontSize: 16,
+  },
+});
